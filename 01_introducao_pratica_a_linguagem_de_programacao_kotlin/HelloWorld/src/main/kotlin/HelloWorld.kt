e6a0b2bf206095f3c7639eb01f63977497a64f4b
Fernando da Costa Felício <ffeliciodeveloper@gmail.com>
2023-08-26 12:51:17 -0300

    feat: usando a função main

---
+++ 01_introducao_pratica_a_linguagem_de_programacao_kotlin/HelloWorld/src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,11 @@
+// O pacote é muito utilizado para organização do projeto
+// Serve também para proteger a visibilidade dos arquivos/classes
+package br.com.dio.estudos
+
+fun main() {
+    println("Teste e quebra a linha") // mostra o conteúdo e quebra a linha
+    println("Teste")
+
+    print("Deixa na mesma linha ") // mostra o conteúdo e não quebra a linha
+    print("Deixa na mesma linha - parte 2")
+}
