c174c84ca267fa18bd94e185e851d3e4193fdbe1
ffelicio82 <fernando_felicio82@yahoo.com.br>
2023-09-07 12:22:19 -0300

    feat: estudo do when (statement e expression)

---
+++ 02_estruturas_de_controle_de_fluxo_e_colecoes_em_kotlin/ControleDeFluxo/src/main/kotlin/WhenStatement.kt
@@ -0,0 +1,33 @@
+import br.com.teste.fernando.MinhaClasse
+
+/*
+ * When
+ *
+ * Pode ser usado no lugar de estrutura de switch
+ * Usada com statement e expressão
+ */
+
+fun main() {
+    casos(objeto = 1) // irá mostrar o valor 'Um'
+    casos(objeto = "Olá") // irá mostrar o valor 'É uma Saudação'
+    casos(objeto = 0L) // irá mostrar o valor 'é um número Long'
+    casos(objeto = MinhaClasse()) // irá mostrar o valor 'Não é uma string'
+
+    /*
+     * irá mostrar o valor 'Argumento Desconhecido'.
+     * Por mais que seja a mesma label de 'Olá', não irá cair na verificação que mostra 'É uma Saudação',
+     * isso é devido a consideração do case sensitive (diferença entre letras maiúsculas e minúsculas por exemplo)
+     */
+    casos(objeto = "olá")
+}
+
+// O Any indica que pode receber qualquer coisa (como se fosse o object - Java / C#)
+fun casos(objeto: Any) {
+    when(objeto) {
+        1 -> println("Um")
+        "Olá" -> println("É uma Saudação")
+        is Long -> println("é um número Long")
+        !is String -> println("Não é uma string")
+        else -> println("Argumento Desconhecido") // opcional. Similar ao 'default' do switch case
+    }
+}
